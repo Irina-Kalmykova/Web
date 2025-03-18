72f16855d1553e43802b736b9c94a432b901eca0
irina <1006-2017@mail.ru>
2025-03-18 22:42:03 +0400

    Initial commmit

--- src/test/java/ru/netology/Web/OrderCardTest.java
+++ src/test/java/ru/netology/Web/OrderCardTest.java
@@ -42,18 +42,6 @@ class OrderCardTest {
     @Test
     void shouldTest1() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
-    }
-
-    @Test
-    void shouldTest2() {
-        driver.get("http://localhost:9999");
         WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
         form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Василий");
         form.findElement(By.cssSelector("[data-test-id= 'phone'] input")).sendKeys("+79270000000");
@@ -62,4 +50,16 @@ class OrderCardTest {
         String text = driver.findElement(By.className("alert-success")).getText();
         assertEquals("Ваша заявка успешно отправлена!", text.trim());
     }
-}
+
+        @Test
+        void shouldTest2() {
+            driver.get("http://localhost:9999");
+            List<WebElement> elements = driver.findElements(By.className("input__control"));
+            elements.get(0).sendKeys("Василий");
+            elements.get(1).sendKeys("+79270000000");
+            driver.findElement(By.className("checkbox__box")).click();
+            driver.findElement(By.className("button")).click();
+            String text = driver.findElement(By.className("alert-success")).getText();
+            assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        }
+    }
